0d792ffd07ad395166644f0c1da1981551c26b6c
Matthew Peers <peersmg@gmail.com>
2019-11-28 11:53:59 +0000

    adds failing test to test CI

--- src/test/java/matt/springproject/spring5webapp/Spring5webappApplicationTests.java
+++ src/test/java/matt/springproject/spring5webapp/Spring5webappApplicationTests.java
@@ -3,11 +3,15 @@ package matt.springproject.spring5webapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
 @SpringBootTest
 class Spring5webappApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(true, is(false));
 	}
 
 }
